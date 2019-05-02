31fd68e3566956819b6999681f73287081f8ecc5
Eduardo <eduardo78d@gmail.com>
2019-05-01 20:41:08 -0500

    asientos

--- asientos.js
+++ asientos.js
@@ -1,28 +1,24 @@
 asientos: {
-  data: {
-    asientos: {
-      asientosA: [
-        {id: 'A1', disponible: true, purchased: false}, {id: 'A2', disponible: true, purchased: false}, {id: 'A3', disponible: true, purchased: false}, {id: 'A4', disponible: true, purchased: false}, {id: 'A5', disponible: true, purchased: false},
-        {id: 'A6', disponible: true, purchased: false}, {id: 'A7', disponible: true, purchased: false}, {id: 'A8', disponible: true, purchased: false}, {id: 'A9', disponible: true, purchased: false}, {id: 'A10', disponible: true, purchased: false},
-      ],
+  zonaA: [
+    {id: 'A1', disponible: true, purchased: false}, {id: 'A2', disponible: true, purchased: false}, {id: 'A3', disponible: true, purchased: false}, {id: 'A4', disponible: true, purchased: false}, {id: 'A5', disponible: true, purchased: false},
+    {id: 'A6', disponible: true, purchased: false}, {id: 'A7', disponible: true, purchased: false}, {id: 'A8', disponible: true, purchased: false}, {id: 'A9', disponible: true, purchased: false}, {id: 'A10', disponible: true, purchased: false},
+  ],
 
-      asientosB: [
-        {id: 'B1', disponible: true, purchased: false}, {id: 'B2', disponible: true, purchased: false}, {id: 'B3', disponible: true, purchased: false}, {id: 'B4', disponible: true, purchased: false}, {id: 'B5', disponible: true, purchased: false},
-        {id: 'B6', disponible: true, purchased: false}, {id: 'B7', disponible: true, purchased: false}, {id: 'B8', disponible: true, purchased: false}, {id: 'B9', disponible: true, purchased: false}, {id: 'B10', disponible: true, purchased: false},
-      ],
+  zonaB: [
+    {id: 'B1', disponible: true, purchased: false}, {id: 'B2', disponible: true, purchased: false}, {id: 'B3', disponible: true, purchased: false}, {id: 'B4', disponible: true, purchased: false}, {id: 'B5', disponible: true, purchased: false},
+    {id: 'B6', disponible: true, purchased: false}, {id: 'B7', disponible: true, purchased: false}, {id: 'B8', disponible: true, purchased: false}, {id: 'B9', disponible: true, purchased: false}, {id: 'B10', disponible: true, purchased: false},
+  ],
 
-      asientosC: [
-        {id: 'C1', disponible: true, purchased: false}, {id: 'C2', disponible: true, purchased: false}, {id: 'C3', disponible: true, purchased: false}, {id: 'C4', disponible: true, purchased: false}, {id: 'C5', disponible: true, purchased: false},
-        {id: 'C6', disponible: true, purchased: false}, {id: 'C7', disponible: true, purchased: false}, {id: 'C8', disponible: true, purchased: false}, {id: 'C9', disponible: true, purchased: false}, {id: 'C10', disponible: true, purchased: false},
-        {id: 'C11', disponible: true, purchased: false}, {id: 'C12', disponible: true, purchased: false}, {id: 'C13', disponible: true, purchased: false}, {id: 'C14', disponible: true, purchased: false}, {id: 'C15', disponible: true, purchased: false},
-        {id: 'C16', disponible: true, purchased: false}, {id: 'C17', disponible: true, purchased: false}, {id: 'C18', disponible: true, purchased: false}, {id: 'C19', disponible: true, purchased: false}, {id: 'C20', disponible: true, purchased: false},
-        {id: 'C21', disponible: true, purchased: false}, {id: 'C2', disponible: true, purchased: false}, {id: 'C23', disponible: true, purchased: false}, {id: 'C24', disponible: true, purchased: false}, {id: 'C25', disponible: true, purchased: false},
-      ],
+  zonaC: [
+    {id: 'C1', disponible: true, purchased: false}, {id: 'C2', disponible: true, purchased: false}, {id: 'C3', disponible: true, purchased: false}, {id: 'C4', disponible: true, purchased: false}, {id: 'C5', disponible: true, purchased: false},
+    {id: 'C6', disponible: true, purchased: false}, {id: 'C7', disponible: true, purchased: false}, {id: 'C8', disponible: true, purchased: false}, {id: 'C9', disponible: true, purchased: false}, {id: 'C10', disponible: true, purchased: false},
+    {id: 'C11', disponible: true, purchased: false}, {id: 'C12', disponible: true, purchased: false}, {id: 'C13', disponible: true, purchased: false}, {id: 'C14', disponible: true, purchased: false}, {id: 'C15', disponible: true, purchased: false},
+    {id: 'C16', disponible: true, purchased: false}, {id: 'C17', disponible: true, purchased: false}, {id: 'C18', disponible: true, purchased: false}, {id: 'C19', disponible: true, purchased: false}, {id: 'C20', disponible: true, purchased: false},
+    {id: 'C21', disponible: true, purchased: false}, {id: 'C22', disponible: true, purchased: false}, {id: 'C23', disponible: true, purchased: false}, {id: 'C24', disponible: true, purchased: false}, {id: 'C25', disponible: true, purchased: false},
+  ],
 
-      asientosD: [
-        {id: 'D1', disponible: true, purchased: false}, {id: 'D2', disponible: true, purchased: false}, {id: 'D3', disponible: true, purchased: false}, {id: 'D4', disponible: true, purchased: false}, {id: 'D5', disponible: true, purchased: false},
-        {id: 'D6', disponible: true, purchased: false}, {id: 'D7', disponible: true, purchased: false}, {id: 'D8', disponible: true, purchased: false}, {id: 'D9', disponible: true, purchased: false}, {id: 'D10', disponible: true, purchased: false},
-      ]
-    },
-  },
-}
+  zonaD: [
+    {id: 'D1', disponible: true, purchased: false}, {id: 'D2', disponible: true, purchased: false}, {id: 'D3', disponible: true, purchased: false}, {id: 'D4', disponible: true, purchased: false}, {id: 'D5', disponible: true, purchased: false},
+    {id: 'D6', disponible: true, purchased: false}, {id: 'D7', disponible: true, purchased: false}, {id: 'D8', disponible: true, purchased: false}, {id: 'D9', disponible: true, purchased: false}, {id: 'D10', disponible: true, purchased: false},
+  ]
+},
